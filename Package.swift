8a9b7c30dda7a7b5c505adac33b18e5cacb475c2
Top-100 <91529693+top-100-writer@users.noreply.github.com>
2022-11-02 16:35:30 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TrackerTop100SDK",
+    platforms: [
+            .iOS(.v11),
+        ],
+    products: [
+        .library(
+            name: "TrackerTop100SDK",
+            // Используем таргет обертку над XCFramework и его зависимостями
+            targets: ["TrackerTop100SDK"]),
+        ],
+    dependencies: [
+    ],
+    targets: [
+        // Подключаем наш фреймворк
+        .binaryTarget(name: "TrackerTop100SDK", url: "https://github.com/top-100-writer/top100-tracker-ios-sdk/releases/download/0.0.1/TrackerTop100SDK.xcframework.zip", checksum: "9abab031e185e2207f14fb20d345322f9c382a67b599d7fb84f7a3b4029c9ab7")
+    ]
+)
